1ce5debe93e9704bb3ac9230a601e4b067e2d617
Gregorio <korgan00@gmail.com>
2020-02-10 15:41:49 -0500

    freq selector

---
+++ Interaqtive-learning/Assets/_Project/Scripts/FreqSelector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FreqSelector : MonoBehaviour {
+
+    public class FequencySelectedEvent : UnityEvent<float> {}
+
+    public FequencySelectedEvent FrequencySelected;
+
+    private float _currentFrequency;
+
+    public float currentFrequency
+    {
+        get
+        {
+            return _currentFrequency;
+        }
+        set
+        {
+            if (_currentFrequency != value)
+            {
+                _currentFrequency = value;
+                FrequencySelected?.Invoke(_currentFrequency);
+            }
+        } 
+    }
+
+
+}
