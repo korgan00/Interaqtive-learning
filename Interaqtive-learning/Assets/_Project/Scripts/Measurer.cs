cb731da45531113eeef75f4c1b7a8fc4ee4ecf37
Gregorio <korgan00@gmail.com>
2020-02-10 15:16:45 -0500

    first commit

---
+++ Interaqtive-learning/Assets/_Project/Scripts/Measurer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Measurer : MonoBehaviour {
+
+
+    public float frequency = 4.2f;
+    public float threshold = 0.5f;
+
+    public float GetPowerAt(float freq)
+    {
+
+
+        return 0;
+    }
+
+}
