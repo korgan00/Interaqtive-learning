2f7b9eeb8a9c4444a11811201674a26f9f61b584
y4izus <yaiza.garcia.mm@gmail.com>
2020-02-10 20:15:32 -0500

    Uses a Lorentzian shape to find the frequency peak

--- Interaqtive-learning/Assets/_Project/Scripts/Measurer.cs
+++ Interaqtive-learning/Assets/_Project/Scripts/Measurer.cs
@@ -10,9 +10,14 @@ public class Measurer : MonoBehaviour {
 
     public float GetPowerAt(float freq)
     {
+        // https://qiskit.org/textbook/ch-quantum-hardware/calibrating-qubits-openpulse.html#1.-Finding-the-qubit-frequency-using-a-frequency-sweep-
+        // To get the value of the peak frequency, we will fit the values to a resonance response curve, which is typically a Lorentzian shape.
+        // https://lmfit.github.io/lmfit-py/builtin_models.html#lmfit.models.LorentzianModel
+        // TODO: We use an aproximation. This should be reviewed by a researcher
 
-
-        return 0;
+        public float frequencyPeak = 1/(Mathf.PI * 0.32) * 1/((freq*20)^2 + 1)
+        
+        return frequencyPeak;
     }
 
 }
